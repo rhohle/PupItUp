44d73cb74904a201c4b851239230d77c9b23b169
Rae Hohle <rhohle@macalester.edu>
2016-04-09 19:37:23 -0500

    second go

---
+++ PupItUp/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('welcomeToPupItUpCtrl', function($scope) {
+
+})
+   
+.controller('meetYourFriendForTheDayCtrl', function($scope) {
+
+})
+   
+.controller('meetYouNewFriendForTheDayCtrl', function($scope) {
+
+})
+      
+.controller('cheerUpCtrl', function($scope) {
+
+})
+   
+.controller('cheerUpWithAKittenCtrl', function($scope) {
+
+})
+ 
